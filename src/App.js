8be98f280e727b18653a1aaf03f833b769a17030
lichlp <hao.lip@husky.neu.edu>
2021-01-14 22:38:28 -0500

    master

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Comment from './comment';
 
 function App() {
   const [input, setInput] = useState('');
+  const [name, setName] = useState('');
   const [messages, setMessages] = useState([]);
 
   useEffect(
@@ -14,11 +15,28 @@ function App() {
   const submit = () => {
     setMessages(messages => [...messages, input]);
     setInput('');
+    fetch("https://fourdragon.azurewebsites.net", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({name: name, input: input}),
+    }).then(data => data.json()).then(data => setMessages([...data]));
+
   }
   return (
     <div className='app'>
-      <input className='input' type='text' value={input} onChange={e => setInput(e.target.value)} />
-      <button onClick={submit}>Submit</button>
+      <div>
+        <label htmlFor={name} >恁滴名字</label>
+        <input id='name' type='text' value={name} onChange={e => setName(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor={name} >恁想说滴话</label>
+        <textarea className='input' type='text' value={input} onChange={e => setInput(e.target.value)} />
+        <button onClick={submit}>Submit</button>
+      </div>
+      <hr />
+
       {
         messages.map(
           (value) => (
