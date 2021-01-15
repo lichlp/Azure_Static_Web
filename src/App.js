678e7aec921ba4945ac66dd81b2de8192e419695
lichlp <hao.lip@husky.neu.edu>
2021-01-14 19:35:42 -0500

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  const submit = () => {
+    setMessages(messages => [...messages, input]);
+    setInput('');
+  }
+  return (
+    <div className='app'>
+      <input className='input' type='text' value={input} onChange={e=>setInput(e.target.value)}/>
+      <button onClick={submit}>Submit</button>
+      <ol>
+        {
+          messages.map(
+            (value) => (
+              <li>
+                {value}
+              </li>
+            )
+          )
+        }
+      </ol>
+    </div>
+  )
 }
 
 export default App;
